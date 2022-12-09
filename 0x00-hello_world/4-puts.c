e801cf2583690b3737dad115874ba34d0d21db28
Adanne33 <adannentobari@gmail.com>
2022-12-09 05:46:19 +0100

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #!bin/bash/
-/**                                                                                                                                     
- * main - Entry                                                                                                                         
- * Return: Always 0                                                                                                                     
+#incude <stdio.h>
+
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ *Return: 0 if exited properly. non-zero otherwise
  */
 int main(void)
 {
-  puts("\"Programming is like building a multi lingual puzzle);                                                           
-        return (0);
-}
+    puts("\"Programing is like building a multilingual puzzle");
+    return (0);
+}    
