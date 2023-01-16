73c2333cfbb9be5674410b444a047793ac09fcb3
Adanne33 <adannentobari@gmail.com>
2023-01-16 07:12:42 -0800

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,7 +18,6 @@ struct dog
 /**
  * dog_t -Typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -26,4 +25,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /* struct dog */
+#endif
